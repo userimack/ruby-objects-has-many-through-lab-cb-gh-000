c7ca1c0775c9859e5738c5cf6f6cb48720d1a417
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 11:31:08 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -5,4 +5,9 @@ class Doctor
     @name = name 
     @appointments = []
   end 
+  
+  def add_appointment(appointment)
+    @appointments << appointment 
+    appointment.doctor = self 
+  end
 end 
